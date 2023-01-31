83118f60f25be4a7e0293ea7220eb2b9d931e7f6
TianaGabel <Tianamg78@gmail.com>
2023-01-31 10:27:35 -0800

    Created Accounts Payable

--- AccountsPayable/src/AccountsPayable.java
+++ AccountsPayable/src/AccountsPayable.java
@@ -5,91 +5,105 @@ public class AccountsPayable {
 	static ArrayList<Employee> payableEmployees = new ArrayList<>();
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Create 6 employees: \n  [1]Salaried Employee\n  [2]Commission Employee "
 				+ "\n  [3]Base + Commission Employee\n  [4]Hourly Employee");
 		while (payableEmployees.size() < 6) {
 			System.out.println("Enter employee you wish to create:  \r");
+			while (!(scan.hasNextInt())) {
+				System.out.println("Enter a number between 1 and 4");
+				scan.next();
+			}
 			int userNum = scan.nextInt();
 			if ((userNum > 4) || (userNum < 1)) {
 				System.out.println("Enter a number between 1 and 4");
 				continue;
 			}
+			// throw away empty character, to allow for inputting names with spaces
+			scan.nextLine();
 			System.out.println("Enter First Name:");
-			String firstName = scan.next();
+			String firstName = scan.nextLine();
 			System.out.println("Enter Last Name:");
-			String lastName = scan.next();
+			String lastName = scan.nextLine();
 			System.out.println("Enter Social Security Num(enter as a number no dashes):");
 			int ssn = scan.nextInt();
 			if (userNum == 1) {
-				//call method to create Salaried Employee
+				// call method to create Salaried Employee
 				System.out.println("Enter weekly Salary in Dollars:");
-				double salary  = scan.nextDouble();
+				double salary = scan.nextDouble();
 				SalariedEmployee salariedEmployee = new SalariedEmployee(firstName, lastName, ssn, salary);
 				payableEmployees.add(salariedEmployee);
-			} else if (userNum == 2){
+			} else if (userNum == 2) {
 				// creates Commission Employee
 				System.out.println("Enter commission rate in Dollars:");
-				double commissionRate  = scan.nextDouble();
+				double commissionRate = scan.nextDouble();
 				System.out.println("Enter gross sales:");
-				double grossSales  = scan.nextDouble();
-				CommissionEmployee commissionEmployee = new CommissionEmployee(firstName, lastName, ssn, grossSales, commissionRate);
+				double grossSales = scan.nextDouble();
+				CommissionEmployee commissionEmployee = new CommissionEmployee(firstName, lastName, ssn, grossSales,
+						commissionRate);
 				payableEmployees.add(commissionEmployee);
 			} else if (userNum == 3) {
-				//creates Base + Commmision Employee
+				// creates Base + Commmision Employee
 				System.out.println("Enter Base pay:");
-				double basePay  = scan.nextDouble();
+				double basePay = scan.nextDouble();
 				System.out.println("Enter commission rate in Dollars:");
-				double commissionRate  = scan.nextDouble();
+				double commissionRate = scan.nextDouble();
 				System.out.println("Enter gross sales:");
-				double grossSales  = scan.nextDouble();
-				
-				BasePlusCommissionEmployee cbEmployee = new BasePlusCommissionEmployee(firstName, lastName, ssn, grossSales, commissionRate, basePay);
+				double grossSales = scan.nextDouble();
+
+				BasePlusCommissionEmployee cbEmployee = new BasePlusCommissionEmployee(firstName, lastName, ssn,
+						grossSales, commissionRate, basePay);
 				payableEmployees.add(cbEmployee);
-				
+
 			} else if (userNum == 4) {
-				//creates Hourly Employee
+				// creates Hourly Employee
 				System.out.println("Enter hourly rate in Dollars:");
-				double hourlyRate  = scan.nextDouble();
+				double hourlyRate = scan.nextDouble();
 				System.out.println("Enter number of Hours worked:");
-				double hours  = scan.nextDouble();
+				double hours = scan.nextDouble();
 				HourlyEmployee hEmployee = new HourlyEmployee(firstName, lastName, ssn, hourlyRate, hours);
 				payableEmployees.add(hEmployee);
-				
+
 			}
 			System.out.println("Employee created!");
 		}
-		//print employee name and payment amount
-		// TODO   have it output 2 zeros
-		System.out.println("\nName               Payment Amount");
+
+		// Prints out all employees
+		System.out.println("\nName                       Payment Amount");
 		for (Employee employee : payableEmployees) {
-			System.out.print("" + employee.getFirstName() + " " + employee.getLastName());
-			System.out.println(" -- $" + employee.getPaymentAmount());
+			printEmployeeWOString(employee);
 		}
-		
+
+		// Checks if employee is a Base plus Commission Employee
 		for (Employee employee : payableEmployees) {
 			if (employee instanceof BasePlusCommissionEmployee) {
 				BasePlusCommissionEmployee currBPC = (BasePlusCommissionEmployee) employee;
 				// increases base pay by 10%
-				currBPC.setBasePay(currBPC.getBasePay()*1.10);
+				currBPC.setBasePay(currBPC.getBasePay() * 1.10);
 				System.out.println("Base + Commission Employee Salary increased by 10%");
 			}
 		}
-		
-		System.out.println("\nName               Payment Amount");
+		System.out.println("\nIncreasing Base pay by 10% for all Base + Commission Employees");
+
+		// Prints out all employees
+		System.out.println("\nName                       Payment Amount");
 		for (Employee employee : payableEmployees) {
-			System.out.print("" + employee.getFirstName() + " " + employee.getLastName());
-			System.out.println(" -- $" + employee.getPaymentAmount());
+			printEmployeeWOString(employee);
 		}
+
+		scan.close();
 	}
-	
+
+	// prints out using the toString method, prints SSN
 	public static void printEmployeeString(Employee employee) {
 		System.out.println(employee.toString());
 	}
-	
+
+	// prints out Name and payment amount, does not print SSN
 	public static void printEmployeeWOString(Employee employee) {
-		System.out.println("" + employee.toString());
+		System.out.printf(" %-10s %-10s ", employee.getFirstName(), employee.getLastName());
+		System.out.printf(" -- $%8.2f\n", employee.getPaymentAmount());
 	}
 
 }
---
+++ HelloWorld/src/FileReaderExample.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+public class FileReaderExample {
+
+	
+	
+	public static void main(String[] args) throws ArrayIndexOutOfBoundsException{
+		File daFile = null;
+		Scanner input = null;
+		
+		
+		//File reading Exception
+		
+		try {
+			daFile = new File("SampleInput");
+			input = new Scanner(daFile);
+			System.out.println(input.nextLine());
+			//This is a file in a different location, you can look up the path from file explorer
+			File otherFile =  new File("src\\SampleInput.txt");
+			Scanner oScan = new Scanner(otherFile);
+			System.out.println(oScan.nextLine());
+		} catch (Exception e) {
+			System.out.println("ex 1: Something went wrong");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		//File ArrayException
+		try {
+			int[] myNums = {1,2,3};
+			//System.out.println(myNums[10]);;
+		} catch (Exception e) {
+			System.out.println("ex 2: Something went wrong");
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		
+		
+	}
+}
