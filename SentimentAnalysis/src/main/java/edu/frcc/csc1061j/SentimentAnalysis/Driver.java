1103d515801ee5bd595f8785ac3410e5c499b7fc
TianaGabel <Tianamg78@gmail.com>
2023-03-21 01:57:43 -0700

    Added loop, tested

--- SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/Driver.java
+++
@@ -1,8 +0,0 @@
-package edu.frcc.csc1061j.SentimentAnalysis;
-
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
-public class Driver {
-//this is empty
-}
--- SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/SentimentAnalyzer.java
+++ SentimentAnalysis/src/main/java/edu/frcc/csc1061j/SentimentAnalysis/SentimentAnalyzer.java
@@ -11,47 +11,68 @@ public class SentimentAnalyzer {
 	static int totalSentiment = 0;
 	static int numWords = 0;
 	static double avgSentiment = 0;
-	public static void main(String[] args) {	
+
+	public static void main(String[] args) {
 		try {
 			loadSentiments();
 		} catch (FileNotFoundException e) {
 			System.out.println("Could not load sentiments");
 			e.printStackTrace();
 		}
-		analyzeSentiment(intakeReview());
-		
-		System.out.println("There were " + numWords + " words");
-		System.out.println("Total sentiment is" + totalSentiment);
-		System.out.println("The average sentiment of the text was:" + avgSentiment);
-		
+		Scanner input = new Scanner(System.in);
+		String userInput;
+		do {
+			totalSentiment = 0;
+			numWords = 0;
+			avgSentiment = 0;
+			System.out.println("Enter your review:");
+			analyzeSentiment(intakeReview());
+
+			System.out.println("There were " + numWords + " words");
+			System.out.println("Total sentiment is " + totalSentiment);
+			avgSentiment = totalSentiment * 1.0 / numWords;
+			System.out.printf("The average sentiment of the text was: %.2f\n", avgSentiment);
+			System.out.println("------------------------------------------------------------");
+			//System.out.println("Significant words: ");
+			//System.out.println("Average sentiment by significant words: ");
+			System.out.println("Would you like to enter another review, \"0\" to quit");
+			userInput = input.nextLine();
+		} while (!userInput.equals("0"));
+
 	}
-	
-	//This takes in input from the user
+
+	// This takes in input from the user
 	public static String[] intakeReview() {
 		Scanner scan = new Scanner(System.in);
 		String inString = "";
-		
-		//loop will stop if it reachs end of File or the word "END"
-		while(scan.hasNextLine()) {
-			//reads in the line
+
+		// loop will stop if it reachs end of File or the word "END"
+		while (scan.hasNextLine()) {
+			// reads in the line
 			String currLine = scan.nextLine();
 			if (currLine.contains("END")) {
-				//if the word end is found, we only take the part before END
+				// if the word end is found, we only take the part before END
 				currLine = currLine.substring(0, currLine.indexOf("END"));
 				inString += " " + currLine.trim();
 				break;
 			}
-			//leading and trailing white space is trimmed, and a space is added between lines
+			// leading and trailing white space is trimmed, and a space is added between
+			// lines
 			inString += " " + currLine.trim();
 		}
+		inString.trim();
 		String[] words = inString.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+		// The above method adds an empty string to the front of the array
+		// This must be accounted for
+		numWords = words.length - 1;
 		return words;
 	}
-	//This loads list of sentiments
+
+	// This loads list of sentiments
 	public static void loadSentiments() throws FileNotFoundException {
-		sentimentsToInts = new MyHashmap<String,Integer>();
+		sentimentsToInts = new MyHashmap<String, Integer>();
 		Scanner scan = null;
-		//opens file of sentiments
+		// opens file of sentiments
 		File sFile = new File("sentiments.txt");
 		scan = new Scanner(sFile);
 		while (scan.hasNextLine()) {
@@ -62,21 +83,23 @@ public class SentimentAnalyzer {
 			sentimentsToInts.put(info[0], Integer.parseInt(info[1]));
 		}
 	}
-	//This anaylyzes the Sentiments
+
+	// This anaylyzes the Sentiments
 	public static void analyzeSentiment(String[] words) {
-		//an empty string is added to the end of the array
-		//So that the loop functions as expected
-		words = Arrays.copyOf(words, words.length+1);
+		// an empty string is added to the end of the array
+		// So that the loop functions as expected
+		words = Arrays.copyOf(words, words.length + 1);
 		words[words.length - 1] = "";
-		//this will anaylyze Sentiments
-		for(int i=0; i < words.length; i++) {
-			//looks for 2 word pair first
-			Integer value = sentimentsToInts.get(words[i]+words[i+1]);
-			//if it is found then we add the value, and skip the second word
+		// this will anaylyze Sentiments
+		// The loop accounts for an empty string at the begining of the array
+		for (int i = 1; i < words.length - 1; i++) {
+			// looks for 2 word pair first
+			Integer value = sentimentsToInts.get(words[i] + " " + words[i + 1]);
+			// if it is found then we add the value, and skip the second word
 			if (value != null) {
 				i++;
 			} else {
-				//otherwise we look for 
+				// otherwise we look for
 				value = sentimentsToInts.get(words[i]);
 				if (value == null) {
 					continue;
@@ -84,7 +107,7 @@ public class SentimentAnalyzer {
 			}
 			totalSentiment += value;
 		}
-		
+
 	}
 
 }
